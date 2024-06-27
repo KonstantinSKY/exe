362d0e48457d4cbf5d1a123e4ee822a75768927e
SKY <sky012877@gmail.com>
2024-06-26 22:04:07 -0700

    src/bin/exe.rs - Modified :

--- src/bin/exe.rs
+++ src/bin/exe.rs
@@ -1,31 +1,31 @@
 extern crate exe as app;
 
-use clap::{Arg, Command};
-use app::styles;
+use clap::Command;
 
 fn main() {
     // styles::app_started();
 
-    let matches = Command::new("Bash Command Beautiful Executor")
+    let matches = Command::new("Universal Beautiful Executor")
         .version("0.1.0")
         .about("Executes a bash command with an beautiful interface")
         .arg_required_else_help(true)
-        .arg(
-            Arg::new("command")
-                .help("The Bash command to execute")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::new("noconfirm")
-                .help("Skips confirmation")
-                .short('n')
-                .long("noconfirm")
-                .action(clap::ArgAction::SetTrue),
-        )
+        .subcommand(app::sh::commands())
+        .subcommand(app::rust::commands())
+
         .get_matches();
+
+    match matches.subcommand() {
+        Some(("sh", arg_matches)) => handle_sh_subcommand(arg_matches),
+        Some(("rust", arg_matches)) => handle_rust_subcommand(arg_matches),
+        _ => println!("No Function Found"),
+    }
     
-    let command = matches.get_one::<String>("command").unwrap();
-    let noconfirm_flag = matches.get_flag("noconfirm");
-    app::exec(command, noconfirm_flag);
 }
+
+// pub fn arg_no_confirm() -> Arg {
+//     Arg::new("noconfirm")
+//     .help("Skip confirmation flag")
+//     .short('n')
+//     .long("noconfirm")
+//     .action(clap::ArgAction::SetTrue)
+// }
