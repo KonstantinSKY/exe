94214b95b688947e3e77b27f57c5e211447a64a1
SKY <sky012877@gmail.com>
2024-06-26 22:52:28 -0700

    src/rust/mod.rs - Modified :

--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,3 +1,4 @@
+mod install;
 use clap::Command;
 
 #[must_use]
@@ -10,4 +11,14 @@ pub fn commands() -> Command {
                 .about("Rust programming languages and ecosystem")
                 .arg(crate::arg_no_confirm()),
         )
+}
+
+
+pub fn handle(arg_matches: &clap::ArgMatches) {
+    if let Some(("install", sub_arg_matches)) = arg_matches.subcommand() {
+        let noconfirm_flag = sub_arg_matches.get_flag("noconfirm");
+        install::run(noconfirm_flag);
+    } else {
+        println!("No subcommand found for rust");
+    }
 }
