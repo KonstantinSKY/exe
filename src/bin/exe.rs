3b2fee33acd574e012cae19a2c95b8fb5bbd23fb
SKY <sky012877@gmail.com>
2024-06-28 09:43:43 -0700

    src/bin/exe.rs - Modified :

--- src/bin/exe.rs
+++ src/bin/exe.rs
@@ -11,16 +11,16 @@ fn main() {
         .arg_required_else_help(true)
         .subcommand(app::sh::commands())
         .subcommand(app::rust::commands())
-
+        .subcommand(app::mods::commands())
         .get_matches();
 
     match matches.subcommand() {
         Some(("sh", arg_matches)) => app::sh::handle(arg_matches),
         Some(("rust", arg_matches)) => app::rust::handle(arg_matches),
-        
+        Some(("mod", arg_matches)) => app::mods::handle(arg_matches),
+
         _ => println!("No Function Found"),
     }
-    
 }
 
 // pub fn arg_no_confirm() -> Arg {
