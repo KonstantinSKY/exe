e6bdb295c9ffc78dafd21c276a10fe34af948689
SKY <sky012877@gmail.com>
2024-06-29 13:58:02 -0700

    src/rust/create_mod.rs - Added :

---
+++ src/rust/create_mod.rs
@@ -0,0 +1,42 @@
+use crate::styles::{h1, h2};
+use std::{
+    fs::{self, File},
+    path::Path,
+    };
+
+
+pub fn run (source_dir: &str, module_name: &str) -> bool {
+    h1("New module in Rust project");
+    
+    let source_path = Path::new(source_dir);
+    if !source_path.is_dir(){
+        println!("Target Directory '{source_path:?}' does not exist.");
+        return false;
+    }
+
+    let module_path =source_path.join(module_name);
+
+    h2("Creating module directory");
+    match fs::create_dir(&module_path) {
+        Ok(()) => {
+            println!("Successfully created module directory: {module_path:?}");
+            create_module_file(&module_path);
+            true
+        }
+        Err(e) => {
+            eprintln!("Failed to create module directory: {e}");
+            false
+        }
+    }
+}
+
+
+fn create_module_file(target_path: &Path){
+    h2("Creating file mod.rs");
+    let file_path = target_path.join("mod.rs");
+
+    match File::create(&file_path) {
+        Ok(_) => println!("Successfully created file: {file_path:?}"),
+        Err(e) => eprintln!("Failed to create file: {e:?}"),
+    }
+}
