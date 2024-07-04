58c40cc6a460f2121f31ed52ac3598d05601dfb4
SKY <sky012877@gmail.com>
2024-07-04 09:38:19 -0700

    src/sh/mod.rs - Modified :

--- src/sh/mod.rs
+++ src/sh/mod.rs
@@ -1,4 +1,5 @@
 pub mod exec;
+pub mod kill;
 
 use clap::{Arg, Command, ArgMatches};
 
@@ -20,6 +21,30 @@ pub fn handle(arg_matches: &ArgMatches) {
     if let Some(command) = arg_matches.get_one::<String>("command") {
         let noconfirm_flag = arg_matches.get_flag("noconfirm");
         exec::exe(command, noconfirm_flag);
+    } else {
+        eprintln!("Error: Command argument is required");
+    }
+}
+
+
+#[must_use]  // modules commands
+pub fn kill_commands() -> Command {
+    Command::new("kill")
+        .about("Kill process by port")
+        .arg_required_else_help(true)
+        .arg(
+            Arg::new("port")
+                .help("Port number to kill")
+                .required(true)
+                // .index(1),
+        )
+}
+
+
+pub fn kill_handle(arg_matches: &ArgMatches) {
+    if let Some(port) = arg_matches.get_one::<String>("port") {
+    println!("Executing the 'mod' command.");
+        let _ = kill::run(port);
     } else {
         eprintln!("Error: Command argument is required");
     }
