0e00bb6d92420a4a42b96a9a38aab2fc5fe25c76
SKY <sky012877@gmail.com>
2024-07-04 09:38:19 -0700

    src/bin/exe.rs - Modified :

--- src/bin/exe.rs
+++ src/bin/exe.rs
@@ -2,6 +2,8 @@ extern crate exe as app;
 
 use clap::Command;
 
+
+
 fn main() {
     // styles::app_started();
 
@@ -13,22 +15,18 @@ fn main() {
         .subcommand(app::rust::commands())
         .subcommand(app::rust::mod_commands())
         .subcommand(app::rust::app_commands())
+        .subcommand(app::deploy::commands())
+        .subcommand(app::sh::kill_commands())
         .get_matches();
 
     match matches.subcommand() {
         Some(("sh", arg_matches)) => app::sh::handle(arg_matches),
-        Some(("rust", arg_matches)) => app::rust::handle(arg_matches),
+        Some((app::rust::RUST, arg_matches)) => app::rust::handle(arg_matches),
         Some(("mod", arg_matches)) => app::rust::mod_handle(arg_matches),
         Some(("app", arg_matches)) => app::rust::app_handle(arg_matches),
+        Some(("deploy", arg_matches)) => app::deploy::handle(arg_matches),
+        Some(("kill", arg_matches)) => app::sh::kill_handle(arg_matches),
 
         _ => println!("No Function Found"),
     }
 }
-
-// pub fn arg_no_confirm() -> Arg {
-//     Arg::new("noconfirm")
-//     .help("Skip confirmation flag")
-//     .short('n')
-//     .long("noconfirm")
-//     .action(clap::ArgAction::SetTrue)
-// }
