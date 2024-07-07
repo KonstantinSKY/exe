d3bbee24fdb179529a36b6bea9f4336d344ff01a
SKY <sky012877@gmail.com>
2024-07-06 20:19:06 -0700

    src/docker/mod.rs - Added :

---
+++ src/docker/mod.rs
@@ -0,0 +1,18 @@
+pub mod install;
+
+use clap::{ArgMatches, Command};
+
+#[must_use]
+pub fn commands() -> Command {
+    Command::new("docker")
+        .about("docker commands")
+        .arg_required_else_help(true)
+        .subcommand(Command::new("install").about("Docker and ecosystem installing"))
+}
+
+pub fn handle(arg_matches: &ArgMatches) {
+    match arg_matches.subcommand() {
+        Some(("install", _sub_matches)) => install::run(),
+        _ => eprintln!("No valid subcommand found"),
+    }
+}
