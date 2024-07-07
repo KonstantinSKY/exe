91240ca874e23de10d896aba8092804a9ccda695
SKY <sky012877@gmail.com>
2024-07-06 18:36:51 -0700

    src/code/mod.rs - Modified :

--- src/code/mod.rs
+++ src/code/mod.rs
@@ -1,1 +1,18 @@
 pub mod install;
+
+use clap::{ArgMatches, Command};
+
+#[must_use]
+pub fn commands() -> Command {
+    Command::new("code")
+        .about("VS CODE editor commands")
+        .arg_required_else_help(true)
+        .subcommand(Command::new("install").about("Code and ecosystem installing"))
+}
+
+pub fn handle(arg_matches: &ArgMatches) {
+    match arg_matches.subcommand() {
+        Some(("install", _sub_matches)) => install::run(),
+        _ => eprintln!("No valid subcommand found"),
+    }
+}
