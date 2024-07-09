ead42a3c31467a6ab51edd33c382eec8ba71af6e
SKY <sky012877@gmail.com>
2024-07-08 23:30:59 -0700

    fast commit

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -10,14 +10,14 @@ use clap::{ArgMatches, Command};
 #[must_use]
 pub fn commands() -> Command {
     Command::new("linux")
-        .about("linux commands")
+        .about("Linux commands")
         .arg_required_else_help(true)
-        .subcommand(Command::new("mount_work_drive").about("Mount Work Drive"))
+        .subcommand(Command::new("mount_work").about("Mount Work Disk to Work Directory"))
 }
 
 pub fn handle(arg_matches: &ArgMatches) {
     match arg_matches.subcommand() {
-        Some(("mount_work_drive", _sub_matches)) => work_drive::mount(),
+        Some(("mount_work", _sub_matches)) => work_drive::mount(),
         _ => eprintln!("No valid subcommand found"),
     }
 }
--- src/linux/work_drive.rs
+++ src/linux/work_drive.rs
@@ -26,21 +26,9 @@ pub fn mount() {
     };
 
     h2!("Creating work directory: {dir}");
-    exe!(&format!("mkdir -p {dir}; ls -la {dir}"));
-
-    h2!("Mounting {drive} to directory: {DIR}");
-    exe!(&format!("sudo mount '{drive}' '{DIR}'"));
-
-    h2!("Checking mounted drive: {drive}");
-    exe!(&format!("mount | grep {DIR}"));
-    
-    let user = env::var("USER").unwrap_or_else(|_| String::new());
-    h2!("Setting full access to mounted drive for user: {user}");
-    exe!(&format!("sudo chown -R {user}:{user} {dir} && sudo chmod -R 700 {dir} && ls -la {dir}"));
-
+    exe!(&format!("mkdir {dir}; ls -la {dir}"));
 
     H1!("Change fstab file for auto mounting");
- 
     h2!("Retrieving the UUID of the drive: {drive}");
     let uuid = if let Some(uuid) = get_uuid(&drive) {
         println!("UUID: {uuid}");
@@ -49,6 +37,18 @@ pub fn mount() {
         eprintln!("No valid uuid found.");
         return;
     };
+
+
+    
+
+    h2!("Checking mounted drive: {drive}");
+    exe!(&format!("mount | grep {DIR}"), true);
+    
+    let user = env::var("USER").unwrap_or_else(|_| String::new());
+    h2!("Setting full access to mounted drive for user: {user}");
+    exe!(&format!("sudo chown -R {user}:{user} {dir} && sudo chmod -R 700 {dir} && ls -la {dir}"));
+
+
 
     let fstab_content = match fs::read_to_string(FSTAB_PATH) {
         Ok(content) => content,
@@ -71,13 +71,17 @@ pub fn mount() {
     let new_entry = format!("UUID={uuid} {dir} {FS_TYPE} {OPTIONS} {DUMP} {PASS}\n");
 
     h2!("Adding the entry to fstab: {new_entry}");
-    exe!(&format!("echo {new_entry} | sudo tee -a /etc/fstab > /dev/null"));
+    exe!(&format!("echo '{new_entry}' | sudo tee -a /etc/fstab > /dev/null"));
 
+
+    h2!("Verifying fstab file");
+    exe!("sudo findmmt --verify");
+    
     h2!("Reviewing the file {FSTAB_PATH}");
-    exe!(&format!("cat {FSTAB_PATH}"));
+    exe!(&format!("cat {FSTAB_PATH}"), true);
 
-    h2!("System rebooting");
-    exe!("sudo reboot");
+    h2!("Mounting all and check it");
+    exe!("sudo mount -a; mount");
 
 }
 
