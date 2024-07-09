4d4aa28e83431f7de03a7a0d0f29fecbb31d1491
SKY <sky012877@gmail.com>
2024-07-08 23:47:08 -0700

    fast commit

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -13,11 +13,13 @@ pub fn commands() -> Command {
         .about("Linux commands")
         .arg_required_else_help(true)
         .subcommand(Command::new("mount_work").about("Mount Work Disk to Work Directory"))
+        .subcommand(Command::new("update").about("Update Linux System (fast) "))
 }
 
 pub fn handle(arg_matches: &ArgMatches) {
     match arg_matches.subcommand() {
         Some(("mount_work", _sub_matches)) => work_drive::mount(),
+        Some(("update", _sub_matches)) => os::update(),
         _ => eprintln!("No valid subcommand found"),
     }
 }
