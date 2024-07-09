b6bdd3408bb5e00ce7aab63d0e0991994f08fd93
SKY <sky012877@gmail.com>
2024-07-08 18:32:14 -0700

    fast commit

--- src/linux/work_drive.rs
+++ src/linux/work_drive.rs
@@ -1,7 +1,6 @@
 use crate::prelude::*;
+use std::{env, str, fs};
 use std::process::Command;
-use std::str;
-use std::env;
 
 const LABEL: &str = "Work";
 const DIR: &str = "Work";
@@ -9,6 +8,7 @@ const FS_TYPE: &str = "btrfs";
 const OPTIONS: &str = "defaults";
 const DUMP: &str = "0";
 const PASS: &str = "2";
+const FSTAB_PATH: &str = "/etc/fstab";
 
 pub fn mount() {
     let dir = env::var("HOME").unwrap_or_else(|_| String::new()) + "/" + DIR;
@@ -17,33 +17,131 @@ pub fn mount() {
     h2!("Finding the device associated with label {LABEL}");
     cmd!("sudo findfs LABEL={LABEL}");
 
-    // let output = Command::new("sudo")
-    //     .arg("findfs")
-    //     .arg(format!("LABEL={LABEL}"))
-    //     .output();
+    let drive = if let Some(drive) = find_device(LABEL) {
+        println!("Using drive: {drive}");
+        drive
+    } else {
+        println!("No valid drive found.");
+        return;
+    };
 
-    // match output {
-    //     Ok(output) if output.status.success() => {
-    //         let drive = str::from_utf8(&output.stdout).unwrap_or("").trim();
-    //         if drive.is_empty() {
-    //             eprintln!("No drive found with label: {LABEL}");
-    //         } else {
-    //             println!("Drive found: {drive}");
-    //         }
-    //     }
-    //     Ok(output) => eprintln!(
-    //         "findfs command failed with error: {}",
-    //         String::from_utf8_lossy(&output.stderr)
-    //     ),
-    //     Err(e) => eprintln!("Failed to execute findfs: {e}"),
-    // }
+    h2!("Creating work directory: {dir}");
+    exe!(&format!("mkdir -p {dir}; ls -la {dir}"), true);
+
+    h2!("Mounting {drive} to directory: {DIR}");
+    exe!(&format!("sudo mount '{drive}' '{DIR}'"));
+
+    h2!("Checking mounted drive: {drive}");
+    exe!(&format!("mount | grep {DIR}"));
+    
+    let user = env::var("USER").unwrap_or_else(|_| String::new());
+    h2!("Setting full access to mounted drive for user: {user}");
+    exe!(&format!("sudo chown -R {user}:{user} {dir} && sudo chmod -R 700 {dir} && ls -la {dir}"));
 
 
-    h2!("Creating work directory: {DIR}");
-    exe!(&format!("echo {DIR}"), true);
+    H1!("Change fstab file for auto mounting");
+ 
+    h2!("Retrieving the UUID of the drive: {drive}");
+    let uuid = if let Some(uuid) = get_uuid(&drive) {
+        println!("UUID: {uuid}");
+        uuid
+    } else {
+        println!("No valid uuid found.");
+        return;
+    };
 
-    // h2(&format!("Mounting {drive} to directory: {DIR}"));
-    // exe(&format!("sudo mount '{drive}' '{DIR}'"), false);
+    let fstab_content = fs::read_to_string(FSTAB_PATH).expect("Unable to read /etc/fstab");
+    if fstab_content.contains(&uuid) {
+        println!("An entry for this UUID ({uuid}) already exists in /etc/fstab.");
+        return;
+    }
+    // Backup the current fstab
+    h2!("Backup the current fstab");
+    exe!(&format!("cp {FSTAB_PATH} {FSTAB_PATH}.bak"));
+
+    // Add the entry to fstab
+    println!("h2 Adding the entry to fstab");
+    let new_entry = format!("UUID={uuid} {dir} {FS_TYPE} {OPTIONS} {DUMP} {PASS}\n");
+
+    h2!("Adding the entry to fstab: {new_entry}");
+    exe!(&format!("echo {new_entry} | sudo tee -a /etc/fstab > /dev/null"));
+
+    h2!("Reviewing the file {FSTAB_PATH}");
+    exe!(&format!("cat {FSTAB_PATH}"));
+
+    h2!("System rebooting");
+    exe!("sudo reboot");
+
+}
+
+
+fn find_device(label: &str) -> Option<String> {
+    let output = Command::new("sudo")
+        .arg("findfs")
+        .arg(format!("LABEL={label}"))
+        .output();
+
+    match output {
+        Ok(output) if output.status.success() => {
+            let drive = str::from_utf8(&output.stdout).unwrap_or("").trim();
+            if drive.is_empty() {
+                eprintln!("No drive found with label: {label}");
+                None
+            } else {
+                println!("Drive found: {drive}");
+                Some(drive.to_string())
+            }
+        }
+        Ok(output) => {
+            eprintln!(
+                "findfs command failed with error: {}",
+                String::from_utf8_lossy(&output.stderr)
+            );
+            None
+        }
+        Err(e) => {
+            eprintln!("Failed to execute findfs: {e}");
+            None
+        }
+    }
+}
+
+fn get_uuid(drive: &str) ->Option<String>{
+    let output = Command::new("sudo")
+    .arg("blkid")
+    .arg("-s")
+    .arg("UUID")
+    .arg("-o")
+    .arg("value")
+    .arg(drive)
+    .output();
+
+    match output {
+        Ok(output) if output.status.success() => {
+            match str::from_utf8(&output.stdout) {
+                Ok(uuid) => {
+                    if uuid.trim().is_empty() {
+                        eprintln!("UUID not found for drive: {drive}");
+                        None
+                    } else {
+                        Some(uuid.to_string())
+                    }
+                }
+                Err(err) => {
+                    eprintln!("Failed to parse UUID output for drive {drive}: {err}");
+                    None
+                }
+            }
+        }
+        Ok(output) => {
+            eprintln!("blkid command failed for drive {drive}: {}", String::from_utf8_lossy(&output.stderr));
+            None
+        }
+        Err(err) => {
+            eprintln!("Failed to execute blkid command for drive {drive}: {err}");
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -51,7 +149,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_run() {
-        mount();
+    fn test_find_device() {
+        find_device(LABEL);
     }
 }
--- src/styles.rs
+++ src/styles.rs
@@ -1,9 +1,5 @@
 #![allow(dead_code)]
 
-use console::Style;
-use crossterm::style::Stylize;
-use std::env;
-
 #[macro_export]
 macro_rules! H1 {
     ($($arg:tt)*) => {
@@ -24,62 +20,3 @@ macro_rules! cmd {
         println!("{}: {}", "Command".blue(), format!($($arg)*).white());
     };
 }
-// pub trait ApplyStyle {
-//     fn green(self) -> String;
-//     fn cyan(self) -> String;
-//     fn blue(self) -> String;
-//     fn red(self) -> String;
-//     fn yellow(self) -> String;
-//     fn white(self) -> String;
-//     fn white_bold(self) -> String;
-//     fn magenta_bold(self) -> String;
-//     fn bold(self) -> String;
-// }
-
-// impl ApplyStyle for &str {
-//     fn green(self) -> String {
-//         Style::new().green().apply_to(self).to_string()
-//     }
-//     fn cyan(self) -> String {
-//         Style::new().cyan().apply_to(self).to_string()
-//     }
-//     fn blue(self) -> String {
-//         Style::new().blue().apply_to(self).to_string()
-//     }
-//     fn red(self) -> String {
-//         Style::new().red().apply_to(self).to_string()
-//     }
-//     fn yellow(self) -> String {
-//         Style::new().yellow().apply_to(self).to_string()
-//     }
-//     fn white(self) -> String {
-//         Style::new().white().apply_to(self).to_string()
-//     }
-//     fn bold(self) -> String {
-//         Style::new().bold().apply_to(self).to_string()
-//     }
-//     fn white_bold(self) -> String {
-//         Style::new().white().bold().apply_to(self).to_string()
-//     }
-//     fn magenta_bold(self) -> String {
-//         Style::new().magenta().bold().apply_to(self).to_string()
-//     }
-// }
-
-// pub fn h1(header_string: &str) {
-//     println!("\n{}", header_string.to_uppercase().white_bold());
-// }
-
-// pub fn h2(header_string: &str) {
-//     println!("\n{} ...", header_string.on_white() .white());
-// }
-
-// pub fn app_started() {
-
-//     let text = "this ".on_white();
-    
-//     match env::args().next() {
-//         Some(binary_name) => println!("Application {binary_name} started"),
-//         None => println!("Failed to retrieve the application name."),
-//     }
-// }
