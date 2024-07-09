85b4a2f9842dfebbf567f4273f3da8fd436562be
SKY <sky012877@gmail.com>
2024-07-09 13:09:19 -0700

    fast commit

--- src/linux/os.rs
+++ src/linux/os.rs
@@ -1,9 +1,7 @@
-
 use crate::{prelude::*, sh};
 use std::fs;
-use std::process::Command;
 use std::path::Path;
-
+use std::process::Command;
 
 use super::manjaro;
 
@@ -15,7 +13,7 @@ pub fn update() {
     }
 }
 
-pub fn install(packages: &str){
+pub fn install(packages: &str) {
     match get().as_str() {
         "Manjaro" => manjaro::packages::install(packages),
         "Unknown" => println!("Unknown operating system"),
@@ -23,7 +21,7 @@ pub fn install(packages: &str){
     }
 }
 
-pub fn mirrors(){
+pub fn mirrors() {
     match get().as_str() {
         "Manjaro" => manjaro::packages::get_mirrors(),
         "Unknown" => println!("Unknown operating system"),
@@ -31,22 +29,27 @@ pub fn mirrors(){
     }
 }
 
-pub fn setup(){
+pub fn setup() {
     H1!("Linux setup");
 
     h2!("Cloning config repository to Work directory");
-    let home_dir = env::var("HOME").unwrap_or_else(|_| String::new());
-    let home_dir_path=Path::new(&home_dir);
+    let home_dir = get_home_dir();
+    let home_dir_path = Path::new(&home_dir);
     let work_dir_path = Path::new(&home_dir).join(WORK_DIR);
     let work_dir = work_dir_path.to_str().unwrap_or("");
-    
-    exe!(&format!("git clone {CONFIG_REPO} {work_dir}; ls -la {work_dir}"));
+
+    exe!(&format!(
+        "git clone {CONFIG_REPO} {work_dir}; ls -la {work_dir}"
+    ));
     h2!("Creating symbolic links to main directories");
     for dir in MAIN_DIRS {
+        if dir.is_empty() {
+            continue;
+        }
         println!("For {}", dir.green());
         let link_path = home_dir_path.join(dir);
         let source_path = work_dir_path.join(dir);
-        
+
         if is_empty_dir(&link_path) {
             println!("Found Empty Directory, will be deleted");
             exe!(&format!("rm {link_path:?} -r"), true);
@@ -59,20 +62,21 @@ pub fn setup(){
             println!("{dir} is file, will be skip");
             continue;
         }
-        if source_path.exists() && !source_path.is_dir(){
+        if source_path.exists() && !source_path.is_dir() {
             println!("Source path is Exists and not Directory, will be skipped");
             continue;
         }
         let source = source_path.to_str().unwrap_or("");
         let link = link_path.to_str().unwrap_or("");
 
-        exe!(&format!("mkdir -p {source}"), true);        
+        exe!(&format!("mkdir -p {source}"), true);
         sh::files::slink(source, link);
+        setup_rc();
     }
 }
-   
+
 fn is_empty_dir(path: &Path) -> bool {
-    if !path.is_dir(){
+    if !path.is_dir() {
         return false;
     };
 
@@ -82,14 +86,48 @@ fn is_empty_dir(path: &Path) -> bool {
     }
 }
 
+fn setup_rc() {
+    H1!("Setting up rc files");
+    let rc_path = Path::new(&get_home_dir()).join(WORK_DIR).join(MAIN_RC);
+    let include_string = format!(". {rc_path:?}"); 
+
+    for rc_file in RC_FILES {
+        h2!("For {}", rc_file.green());
+        let target_file = ".".to_owned() + rc_file;
+        let target_path = Path::new(&get_home_dir()).join(target_file);
+        if !target_path.exists() {
+            exe!(&format!("touch {target_path:?}"));
+        }
+        if !target_path.is_file() {
+            println!("{target_path:?} is not file, skipping)");
+            continue;
+        }
+        let file_content = match fs::read_to_string(&target_path) {
+            Ok(content) => content,
+            Err(e) => {
+                println!("Unable to read file. Error: {e}");
+                continue;
+            }
+        };
+        h2!("Adding link string");
+        if !file_content.contains(&include_string) {
+            exe!(&format!("echo {include_string} | tee -a {target_path:?}"));
+        }
+
+        h2!("Checking if added");
+        exe!(&format!("tail -n 2 {target_path:?}"), true); 
+    
+    }
+}
+
 fn get() -> String {
-   // Run the hostnamectl command and capture the output
-   let hostname = Command::new("hostnamectl")
-   .output()
-   .expect("Failed to execute command");
+    // Run the hostnamectl command and capture the output
+    let hostname = Command::new("hostnamectl")
+        .output()
+        .expect("Failed to execute command");
 
     let hostname_str = String::from_utf8_lossy(&hostname.stdout);
-    println!("{hostname_str}"); 
+    println!("{hostname_str}");
 
     let os_name = match hostname_str {
         s if s.contains("Ubuntu") => "Ubuntu",
@@ -116,6 +154,6 @@ mod tests {
 
     #[test]
     fn test_install() {
-    install("gimp partitionmanager");
+        install("gimp partitionmanager");
     }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,15 +1,15 @@
-pub use crossterm::style::Stylize; 
+pub use crossterm::style::Stylize;
 
 pub use std::env;
 
-pub use crate::{H1, h2, cmd, exe};
+pub use crate::{cmd, exe, h2, H1};
 
-pub const WORK_DIR: &str ="Work";
+pub const WORK_DIR: &str = "Work";
 pub const CONFIG_REPO: &str = "https://github.com/KonstantinSKY/Configs.git";
 
-pub const MAIN_DIRS: [&str; 15]=[
+pub const MAIN_DIRS: [&str; 15] = [
     "Tools",
-    "Downloads", 
+    "Downloads",
     "Security",
     "Documents",
     "Music",
@@ -24,3 +24,9 @@ pub const MAIN_DIRS: [&str; 15]=[
     "",
     "",
 ];
+pub const MAIN_RC: &str = "Configs/rc";
+pub const RC_FILES: [&str; 3] = ["bashrc", "zhsrc", "zshrc"];
+
+pub fn get_home_dir() -> String {
+    env::var("HOME").unwrap_or_else(|_| String::new())
+}
