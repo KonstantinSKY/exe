2372f5aa118866910b0a42411257f561a7e0eb0c
SKY <sky012877@gmail.com>
2024-07-21 09:41:54 -0700

    fast commit

--- src/js/config.rs
+++ src/js/config.rs
@@ -1,26 +1,26 @@
 
-#[derive(Deserialize, Debug)]
-pub struct Config {
-    pub packages: String,
-}
+// #[derive(Deserialize, Debug)]
+// pub struct Config {
+//     pub packages: String,
+// }
 
-impl Config {
-    fn new(path: &Path) -> Self {
-        crate::configs::read_and_parse_toml(path)
-    }
-}
+// impl Config {
+//     fn new(path: &Path) -> Self {
+//         crate::configs::read_and_parse_toml(path)
+//     }
+// }
 
-#[derive(Deserialize, Debug)]
-pub struct Packages {
-    pub unneeded: String,
-    pub requirements: String,
-    pub requirements_2: String,
-    pub internet: String,
-    pub communication: String,
-}
+// #[derive(Deserialize, Debug)]
+// pub struct Packages {
+//     pub unneeded: String,
+//     pub requirements: String,
+//     pub requirements_2: String,
+//     pub internet: String,
+//     pub communication: String,
+// }
 
-#[must_use]
-pub fn get(key:&str) -> Config{
-    let config_source_path = crate::configs::get_config_path(key);
-    Config::new(&config_source_path)
-}
+// #[must_use]
+// pub fn get(key:&str) -> Config{
+//     let config_source_path = crate::configs::get_config_path(key);
+//     Config::new(&config_source_path)
+// }
