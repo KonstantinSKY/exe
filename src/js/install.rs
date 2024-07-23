4c8b16b1ac98b0bf72ca773b6cf50cce14be4f02
sky <sky012877@gmail.com>
2024-07-22 23:36:54 -0700

    fast commit

--- src/js/install.rs
+++ src/js/install.rs
@@ -1,55 +1,46 @@
-use crate::prelude::*;
 use crate::linux::manjaro::packages::install;
+use crate::prelude::*;
 
-
-pub fn run (){
+pub fn run() {
     H1!("JavaScript Eco System");
 
-h2!("Installing needed packages");
-install("nodejs npm yarn nvm");
+    h2!("Installing needed packages");
+    install("nodejs npm yarn nvm");
 
-h2!("Activating NVM");
+    h2!("Activating NVM");
 
-exe!("source /usr/share/nvm/init-nvm.sh");
+    let nvm_init = "source /usr/share/nvm/init-nvm.sh";
 
-h2!("Showing versions");
-exe!("node --version; npm --version;  yarn --version; nvm --version"; true);
+    h2!("Showing versions");
+    exe!("node --version; npm --version;  yarn --version; {nvm_init}; nvm --version"; true);
 
-// H1!("NVM - Node Version Manager");
-// h2!("Installing nvm");
-// exe!("wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash");
+    // H1!("NVM - Node Version Manager");
+    // h2!("Installing nvm");
+    // exe!("wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash");
 
-// h2!("Exporting variables");
-// exe!("export NVM_DIR='$HOME/.nvm'");
-// exe!(r"[ -s '$NVM_DIR/nvm.sh' ] && \. '$NVM_DIR/nvm.sh'"; true);
+    // h2!("Exporting variables");
+    // exe!("export NVM_DIR='$HOME/.nvm'");
+    // exe!(r"[ -s '$NVM_DIR/nvm.sh' ] && \. '$NVM_DIR/nvm.sh'"; true);
 
-h2!("Reloading terminal");
-exe!("source $HOME/.bashrc");
+    // H1!("NODE.JS VERSIONS");
+    // h2!("Node remote LTS versions");
+    // exe!("nvm ls-remote --lts"; true);
 
-// H1!("NODE.JS VERSIONS");
-// h2!("Node remote LTS versions");
-// exe!("nvm ls-remote --lts"; true);
+    // h2!("Node Installed versions");
+    // exe!("nvm ls"; true);
 
-// h2!("Node Installed versions");
-// exe!("nvm ls"; true);
+    // h2!("Installing NodeJS latest LTS version, switch to and showing all installed versions");
+    // exe!("nvm install --lts; nvm use --lts");
 
-// h2!("Installing NodeJS latest LTS version, switch to and showing all installed versions");
-// exe!("nvm install --lts; nvm use --lts");
+    // h2!("Node Installed versions:");
+    // exe!("nvm ls"; true);
+    // h2!("Current activated version:");
+    // exe!("nvm current"; true);
 
-// h2!("Node Installed versions:");
-// exe!("nvm ls"; true);
-// h2!("Current activated version:");
-// exe!("nvm current"; true);
+    // H1!("NPM  versions");
+    // h2!("Checking npm version for update");
+    // exe!("npm -g outdated npm"; true);
 
-// H1!("NPM  versions");
-// h2!("Checking npm version for update");
-// exe!("npm -g outdated npm"; true);
-
-// h2!("Update npm for latest version");
-// exe!("npm install -g npm@latest; npm --version");
-
-
-
-
-
+    // h2!("Update npm for latest version");
+    // exe!("npm install -g npm@latest; npm --version");
 }
